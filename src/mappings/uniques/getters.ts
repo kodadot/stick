ff1f29e29a6fca921f7f8524ca6ab4da1dc55246
Viki Val <viktorko99@gmail.com>
2023-04-27 16:33:53 +0200

    :zap: 1:1 with snek

---
+++ src/mappings/uniques/getters.ts
@@ -0,0 +1,182 @@
+
+import { Interaction } from '../../model'
+import * as events from '../../types/events'
+import { addressOf } from '../utils/helper'
+import { warn } from '../utils/logger'
+import { Context } from '../utils/types'
+import { BurnTokenEvent, BuyTokenEvent, CreateCollectionEvent, CreateTokenEvent, DestroyCollectionEvent, ListTokenEvent, TransferTokenEvent } from './types'
+
+export function getCreateCollectionEvent(ctx: Context): CreateCollectionEvent {
+  const event = new events.UniquesCreatedEvent(ctx)
+  if (event.isV1) {
+    const [classId, creator, owner] = event.asV1
+    return { id: classId.toString(), caller: addressOf(creator), owner: addressOf(owner) }
+  }
+  if (event.isV700) {
+    const { class: classId, creator, owner } = event.asV700
+    return { id: classId.toString(), caller: addressOf(creator), owner: addressOf(owner) }
+  }
+  if (event.isV9230) {
+    const { collection: classId, creator, owner } = event.asV9230
+    return { id: classId.toString(), caller: addressOf(creator), owner: addressOf(owner) }
+  }
+  warn(Interaction.CREATE, 'USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    creator,
+    owner,
+  } = ctx._chain.decodeEvent(ctx.event)
+  return { id: classId.toString(), caller: addressOf(creator), owner: addressOf(owner) }
+}
+
+export function getCreateTokenEvent(ctx: Context): CreateTokenEvent {
+  const event = new events.UniquesIssuedEvent(ctx)
+  if (event.isV1) {
+    const [classId, instanceId, owner] = event.asV1
+    return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+  }
+  if (event.isV700) {
+    const { class: classId, instance: instanceId, owner } = event.asV700
+    return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+  }
+  if (event.isV9230) {
+    const { collection: classId, item: instanceId, owner } = event.asV9230
+    return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+  }
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    item: instanceId,
+    owner,
+  } = ctx._chain.decodeEvent(ctx.event)
+  return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+}
+
+export function getTransferTokenEvent(ctx: Context): TransferTokenEvent {
+  const event = new events.UniquesTransferredEvent(ctx)
+  if (event.isV1) {
+    const [classId, instanceId, from, to] = event.asV1
+    return { collectionId: classId.toString(), caller: addressOf(from), sn: instanceId.toString(), to: addressOf(to), }
+  }
+  if (event.isV700) {
+    const { class: classId, instance: instanceId, from, to } = event.asV700
+    return { collectionId: classId.toString(), caller: addressOf(from), sn: instanceId.toString(), to: addressOf(to), }
+  }
+  if (event.isV9230) {
+    const { collection: classId, item: instanceId, from, to } = event.asV9230
+    return { collectionId: classId.toString(), caller: addressOf(from), sn: instanceId.toString(), to: addressOf(to) }
+  }
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    item: instanceId,
+    from,
+    to,
+  } = ctx._chain.decodeEvent(ctx.event)
+  return { collectionId: classId.toString(), caller: addressOf(from), sn: instanceId.toString(), to: addressOf(to), }
+}
+
+export function getBurnTokenEvent(ctx: Context): BurnTokenEvent {
+  const event = new events.UniquesBurnedEvent(ctx)
+  if (event.isV1) {
+    const [classId, instanceId, owner] = event.asV1
+    return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+  }
+  if (event.isV700) {
+    const { class: classId, instance: instanceId, owner } = event.asV700
+    return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+  }
+  if (event.isV9230) {
+    const { collection: classId, item: instanceId, owner } = event.asV9230
+    return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+  }
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    item: instanceId,
+    owner,
+  } = ctx._chain.decodeEvent(ctx.event)
+  return { collectionId: classId.toString(), owner: addressOf(owner), sn: instanceId.toString() }
+}
+
+export function getDestroyCollectionEvent(ctx: Context): DestroyCollectionEvent {
+  const event = new events.UniquesDestroyedEvent(ctx)
+  if (event.isV1) {
+    const classId = event.asV1
+    return { id: classId.toString() }
+  }
+  if (event.isV700) {
+    const { class: classId } = event.asV700
+    return { id: classId.toString() }
+  }
+  if (event.isV9230) {
+    const { collection: classId } = event.asV9230
+    return { id: classId.toString() }
+  }
+
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const { collection: classId } = ctx._chain.decodeEvent(ctx.event)
+  return { id: classId.toString() }
+}
+
+export function getListTokenEvent(ctx: Context): ListTokenEvent {
+  const event = new events.UniquesItemPriceSetEvent(ctx)
+
+  if (event.isV9270) {
+    const { collection: classId, item: instanceId, price } = event.asV9270
+    return { collectionId: classId.toString(), sn: instanceId.toString(), price }
+  }
+
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    item: instanceId,
+    price,
+  } = ctx._chain.decodeEvent(ctx.event)
+  return { collectionId: classId.toString(), sn: instanceId.toString(), price }
+}
+
+export function getUnListTokenEvent(ctx: Context): ListTokenEvent {
+  const event = new events.UniquesItemPriceRemovedEvent(ctx)
+
+  if (event.isV9270) {
+    const { collection: classId, item: instanceId } = event.asV9270
+    return { collectionId: classId.toString(), sn: instanceId.toString(), price: 0n }
+  }
+
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    item: instanceId,
+    price,
+  } = ctx._chain.decodeEvent(ctx.event)
+  return { collectionId: classId.toString(), sn: instanceId.toString(), price: 0n }
+}
+
+export function getBuyTokenEvent(ctx: Context): BuyTokenEvent {
+  const event = new events.UniquesItemBoughtEvent(ctx)
+
+  if (event.isV9270) {
+    const {
+      collection: classId,
+      item: instanceId,
+      price,
+      seller,
+      buyer,
+    } = event.asV9270
+    return {
+      collectionId: classId.toString(), caller: addressOf(buyer), sn: instanceId.toString(), price: BigInt(price ?? 0), currentOwner: addressOf(seller),
+    }
+  }
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    item: instanceId,
+    price,
+    seller,
+    buyer,
+  } = ctx._chain.decodeEvent(ctx.event)
+  return {
+    collectionId: classId.toString(), caller: addressOf(buyer), sn: instanceId.toString(), price: BigInt(price ?? 0), currentOwner: addressOf(seller),
+  }
+}
---
+++ src/mappings/uniques/types.ts
@@ -0,0 +1,47 @@
+import { createTokenId } from '../utils/types'
+
+export type BaseCollectionEvent = {
+  id: string;
+  caller: string;
+};
+
+export type BaseTokenEvent = {
+  collectionId: string;
+  sn: string;
+};
+
+export type OptionalMeta = {
+  metadata?: string;
+};
+
+export type CreateCollectionEvent = BaseCollectionEvent & OptionalMeta & {
+  owner: string;
+};
+
+export type CreateTokenEvent = BaseTokenEvent & OptionalMeta & {
+  // caller: string; // Not Correct // may you can mint & send in one
+  owner: string;
+  metadata?: string;
+};
+
+export type TransferTokenEvent = BaseTokenEvent & {
+  caller: string;
+  to: string;
+};
+
+export type ListTokenEvent = BaseTokenEvent & {
+  price?: bigint
+};
+
+export type BuyTokenEvent = ListTokenEvent & {
+  currentOwner: string;
+  caller: string;
+};
+
+export type BurnTokenEvent = CreateTokenEvent;
+
+export type DestroyCollectionEvent = {
+  id: string;
+};
+
+export const tokenIdOf = (base: BaseTokenEvent): string => createTokenId(base.collectionId, base.sn);
