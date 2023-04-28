d0fdc917bd829ba9bb257e161c649b3a7faa1c99
Viki Val <viktorko99@gmail.com>
2023-04-28 15:20:12 +0200

    :squid: setting for statemine

--- src/processor.ts
+++ src/processor.ts
@@ -10,11 +10,12 @@ const processor = new SubstrateProcessor(database);
 
 const STARTING_BLOCK = 778425; // 6000 or 1790000 for Prod
 
-processor.setTypesBundle('basilisk');
+processor.setTypesBundle('statemine')
 processor.setBlockRange({ from: STARTING_BLOCK });
 
+// const ARCHIVE = 'https://statemine.archive.subsquid.io/graphql';
 const archive = lookupArchive('statemine', {release: 'FireSquid'} )
-const chain = undefined
+const chain = 'wss://statemine-rpc.polkadot.io'
 
 processor.setDataSource({
     archive,
