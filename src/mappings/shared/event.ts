c33336f41009ff36ad5c08c120a994a3594e93fc
Viki Val <viktorko99@gmail.com>
2023-04-29 14:19:31 +0200

    :tada: create Event

---
+++ src/mappings/shared/event.ts
@@ -0,0 +1,28 @@
+import { create } from '@kodadot1/metasquid/entity'
+import { Store } from '@kodadot1/metasquid/types'
+
+import { nanoid } from 'nanoid'
+import { Event, Interaction, NFTEntity } from '../../model'
+import { error } from '../utils/logger'
+import { Action, BaseCall, eventFrom } from '../utils/types'
+
+
+export const eventId = (id: string, event: Action) => `${id}-${event}${nanoid()}`
+
+export async function createEvent(
+  final: NFTEntity,
+  interaction: Interaction,
+  call: BaseCall,
+  meta: string,
+  store: Store,
+  currentOwner?: string
+) {
+  try {
+    const newEventId = eventId(final.id, interaction)
+    const event = create<Event>(Event, newEventId, eventFrom(interaction, call, meta, currentOwner))
+    event.nft = final
+    await store.save(event)
+  } catch (e) {
+    error(e, interaction, final.id)
+  }
+}
