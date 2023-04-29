c698696ddcd4840f1dcbaaaea837c238413a9c81
Viki Val <viktorko99@gmail.com>
2023-04-30 01:23:25 +0200

    :sparkles: buy

--- src/mappings/uniques/buy.ts
+++ src/mappings/uniques/buy.ts
@@ -1,3 +1,33 @@
-import { Context } from '../utils/types'
+import { getWith } from '@kodadot1/metasquid/entity'
+import {
+  NFTEntity as NE
+} from '../../model'
+import { createEvent } from '../shared/event'
+import { unwrap } from '../utils/extract'
+import { debug, pending, success } from '../utils/logger'
+import { Action, Context, createTokenId } from '../utils/types'
+import { getBuyTokenEvent } from './getters'
 
-export async function handleTokenBuy(context: Context): Promise<void> {}
+const OPERATION = Action.BUY
+
+export async function handleTokenBuy(context: Context): Promise<void> {
+  pending(OPERATION, `${context.block.height}`);
+  const event = unwrap(context, getBuyTokenEvent);
+  debug(OPERATION, event);
+
+
+  const id = createTokenId(event.collectionId, event.sn);
+  const entity = await getWith(context.store, NE, id, { collection: true });
+
+  entity.price = BigInt(0);
+  entity.currentOwner = event.caller;
+  entity.updatedAt = event.timestamp;
+
+  // TODO: update collection Floor
+  // TODO: UPDATE COLLECTION DISTRIBUTION
+
+  success(OPERATION, `${id} by ${event.caller} for ${String(event.price)}`);
+  await context.store.save(entity);
+  const meta = String(event.price || '');
+  await createEvent(entity, OPERATION, event, meta, context.store, event.currentOwner);
+}
