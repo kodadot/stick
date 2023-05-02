a08e9747bbfcce7e98a6bc0faa64eba749b70310
Viki Val <viktorko99@gmail.com>
2023-05-02 17:08:21 +0200

    :zap: return on unknow events

--- src/mappings/uniques/setMetadata.ts
+++ src/mappings/uniques/setMetadata.ts
@@ -1,19 +1,26 @@
-import { getOrFail as get } from '@kodadot1/metasquid/entity'
+import { get as get } from '@kodadot1/metasquid/entity'
 import { unwrap } from '../utils/extract'
 import { Context } from '../utils/types'
 import { getMetadataEvent } from './getters'
 import { CollectionEntity, NFTEntity } from '../../model'
 import { tokenIdOf } from './types'
 import { handleMetadata } from '../shared/metadata'
+import { debug, warn } from '../utils/logger'
 
 export async function handleMetadataSet(context: Context): Promise<void> {
   const event = unwrap(context, getMetadataEvent);
+  debug('METADATA' as any, event)
 
   if (!event.metadata) {
     return;
   }
 
   const final = event.sn !== undefined ? await get(context.store, NFTEntity, tokenIdOf(event as any)) : await get(context.store, CollectionEntity, event.collectionId);
+
+  if (!final) {
+    warn('METADATA' as any, `MISSING ${event.collectionId}-${event.sn}`)
+    return;
+  }
   
   final.metadata = event.metadata;
 
