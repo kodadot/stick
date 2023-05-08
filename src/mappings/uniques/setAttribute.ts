9ad5f0decc6fa235902a0b41ae8bf1b5e41c18c0
Viki Val <viktorko99@gmail.com>
2023-05-08 13:32:48 +0200

    :construction: setAttribute

--- src/mappings/uniques/setAttribute.ts
+++ src/mappings/uniques/setAttribute.ts
@@ -1,29 +1,16 @@
 import { getOrFail as get } from '@kodadot1/metasquid/entity'
+import { CollectionEntity, NFTEntity } from '../../model'
 import { unwrap } from '../utils/extract'
 import { Context } from '../utils/types'
-import { getMetadataEvent } from './getters'
-import { CollectionEntity, NFTEntity } from '../../model'
+import { getAttributeEvent } from './getters'
 import { tokenIdOf } from './types'
-import { handleMetadata } from '../shared/metadata'
 
 export async function handleAttributeSet(context: Context): Promise<void> {
-  const event = unwrap(context, getMetadataEvent);
-
-  if (!event.metadata) {
-    return;
-  }
+  const event = unwrap(context, getAttributeEvent);
 
   const final = event.sn !== undefined ? await get(context.store, NFTEntity, tokenIdOf(event as any)) : await get(context.store, CollectionEntity, event.collectionId);
   
-  final.metadata = event.metadata;
-
-  if (final.metadata) {
-    const metadata = await handleMetadata(final.metadata, context.store);
-    final.meta = metadata;
-    final.name = metadata?.name;
-    final.image = metadata?.image;
-    final.media = metadata?.animationUrl;
-  }
+  // final.attributes = final.attributes?.filter((attr) => attr.key !== event.key.toString());
 
   await context.store.save(final);
 }
