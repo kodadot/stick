09103f3a4ccee9029aebe61263c0d6978c4bed84
Viki Val <viktorko99@gmail.com>
2023-07-18 15:50:54 +0200

    :squid: typegen statemint nfts

--- src/types/statemint/storage.ts
+++ src/types/statemint/storage.ts
@@ -2,6 +2,95 @@ import assert from 'assert'
 import {Block, BlockContext, Chain, ChainContext, Option, Result, StorageBase} from './support'
 import * as v601 from './v601'
 import * as v9230 from './v9230'
+import * as v9430 from './v9430'
+
+export class NftsCollectionStorage extends StorageBase {
+    protected getPrefix() {
+        return 'Nfts'
+    }
+
+    protected getName() {
+        return 'Collection'
+    }
+
+    /**
+     *  Details of a collection.
+     */
+    get isV9430(): boolean {
+        return this.getTypeHash() === '2048922b0682bb5b7de7ca9a7e01264f12386f5c3cad9a3e84b1e5a81cd69913'
+    }
+
+    /**
+     *  Details of a collection.
+     */
+    get asV9430(): NftsCollectionStorageV9430 {
+        assert(this.isV9430)
+        return this as any
+    }
+}
+
+/**
+ *  Details of a collection.
+ */
+export interface NftsCollectionStorageV9430 {
+    get(key: number): Promise<(v9430.Type_353 | undefined)>
+    getAll(): Promise<v9430.Type_353[]>
+    getMany(keys: number[]): Promise<(v9430.Type_353 | undefined)[]>
+    getKeys(): Promise<number[]>
+    getKeys(key: number): Promise<number[]>
+    getKeysPaged(pageSize: number): AsyncIterable<number[]>
+    getKeysPaged(pageSize: number, key: number): AsyncIterable<number[]>
+    getPairs(): Promise<[k: number, v: v9430.Type_353][]>
+    getPairs(key: number): Promise<[k: number, v: v9430.Type_353][]>
+    getPairsPaged(pageSize: number): AsyncIterable<[k: number, v: v9430.Type_353][]>
+    getPairsPaged(pageSize: number, key: number): AsyncIterable<[k: number, v: v9430.Type_353][]>
+}
+
+export class NftsItemMetadataOfStorage extends StorageBase {
+    protected getPrefix() {
+        return 'Nfts'
+    }
+
+    protected getName() {
+        return 'ItemMetadataOf'
+    }
+
+    /**
+     *  Metadata of an item.
+     */
+    get isV9430(): boolean {
+        return this.getTypeHash() === 'a2ef643060836070ede73871794a2c9da331285c79b5e9e6f9935723f37af9c6'
+    }
+
+    /**
+     *  Metadata of an item.
+     */
+    get asV9430(): NftsItemMetadataOfStorageV9430 {
+        assert(this.isV9430)
+        return this as any
+    }
+}
+
+/**
+ *  Metadata of an item.
+ */
+export interface NftsItemMetadataOfStorageV9430 {
+    get(key1: number, key2: number): Promise<(v9430.Type_363 | undefined)>
+    getAll(): Promise<v9430.Type_363[]>
+    getMany(keys: [number, number][]): Promise<(v9430.Type_363 | undefined)[]>
+    getKeys(): Promise<[number, number][]>
+    getKeys(key1: number): Promise<[number, number][]>
+    getKeys(key1: number, key2: number): Promise<[number, number][]>
+    getKeysPaged(pageSize: number): AsyncIterable<[number, number][]>
+    getKeysPaged(pageSize: number, key1: number): AsyncIterable<[number, number][]>
+    getKeysPaged(pageSize: number, key1: number, key2: number): AsyncIterable<[number, number][]>
+    getPairs(): Promise<[k: [number, number], v: v9430.Type_363][]>
+    getPairs(key1: number): Promise<[k: [number, number], v: v9430.Type_363][]>
+    getPairs(key1: number, key2: number): Promise<[k: [number, number], v: v9430.Type_363][]>
+    getPairsPaged(pageSize: number): AsyncIterable<[k: [number, number], v: v9430.Type_363][]>
+    getPairsPaged(pageSize: number, key1: number): AsyncIterable<[k: [number, number], v: v9430.Type_363][]>
+    getPairsPaged(pageSize: number, key1: number, key2: number): AsyncIterable<[k: [number, number], v: v9430.Type_363][]>
+}
 
 export class UniquesClassStorage extends StorageBase {
     protected getPrefix() {
