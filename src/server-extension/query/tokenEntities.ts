b5963dc43ce648a558d478a805fbcba223213362
daiagi <daiagi@gmail.com>
2023-10-10 15:00:03 +0700

    cheapest is already on tokenEntity

--- src/server-extension/query/tokenEntities.ts
+++ src/server-extension/query/tokenEntities.ts
@@ -1,15 +1,4 @@
-export const tokenEntities = `WITH cheapest_nft AS (
-    SELECT 
-        ne.token_id,
-        ne.id AS nft_id,
-        ne.price AS cheapest,
-        ROW_NUMBER() OVER(PARTITION BY ne.token_id ORDER BY ne.price ASC) AS rnk
-    FROM 
-        nft_entity ne
-    WHERE 
-    ($1::text IS NULL OR ne.current_owner = $1) AND 
-    ($7::text[] IS NULL OR ne.issuer NOT IN (SELECT unnest($7)))
-),
+export const tokenEntities = `WITH 
 nft_count AS (
     SELECT 
         token_id, 
@@ -22,6 +11,17 @@ nft_count AS (
     ($7::text[] IS NULL OR ne.issuer NOT IN (SELECT unnest($7)))
     GROUP BY 
         token_id
+),
+ collection_floor_price AS (
+    SELECT
+        collection_id,
+        MIN(price) as floor_price
+    FROM 
+        nft_entity
+    WHERE 
+        burned = false
+    GROUP BY 
+        collection_id
 )
 
 SELECT
@@ -37,20 +37,22 @@ SELECT
     t.block_number as block_number,
     t.created_at AS created_at,
     t.updated_at AS updated_at,
+    c.id as cheapest_id,
+    c.price as cheapest_price,
     nft_count.count as count,
     nft_count.supply as supply,
-    c.nft_id as cheapest_id,
-    c.cheapest as cheapest_price,
     col.id AS collection_id,
-    col.name AS collection_name
+    col.name AS collection_name,
+    cfp.floor_price AS collection_floor_price
 FROM
     token_entity as t
-        JOIN cheapest_nft as c ON t.id = c.token_id AND c.rnk = 1
         JOIN collection_entity as col ON t.collection_id = col.id
         JOIN metadata_entity as me ON t.meta_id = me.id
         JOIN nft_count ON t.id = nft_count.token_id
+        JOIN collection_floor_price as cfp ON t.collection_id = cfp.collection_id
+        LEFT JOIN nft_entity as c ON t.cheapest_id = c.id
 WHERE
-    ($4::bigint IS NULL OR c.cheapest >= $4::bigint) AND
-    ($5::bigint IS NULL OR c.cheapest > $5::bigint) AND
-    ($6::bigint IS NULL OR c.cheapest <= $6::bigint)
+    ($4::bigint IS NULL OR c.price >= $4::bigint) AND
+    ($5::bigint IS NULL OR c.price > $5::bigint) AND
+    ($6::bigint IS NULL OR c.price <= $6::bigint)
 `
