dcdca01405fc689c72a35e2fb4eb33fb81b9127b
Viki Val <viktorko99@gmail.com>
2023-11-19 17:51:10 +0100

    :zap: kinky setup of env

--- src/environment.ts
+++ src/environment.ts
@@ -1,18 +1,18 @@
 import { lookupArchive } from '@subsquid/archive-registry'
 
-export const CHAIN = process.env.CHAIN || 'statemine'
+export const CHAIN = process.env.CHAIN || 'kusama'
 
-// Statemine
-const ARCHIVE_URL = lookupArchive('statemine', { release: 'FireSquid' })
-const NODE_URL = 'wss://statemine-rpc.polkadot.io'
+// Asset Hub
+const ARCHIVE_URL = lookupArchive(`asset-hub-${CHAIN}`, { release: 'ArrowSquid' })
+const NODE_URL = `wss://${CHAIN}-asset-hub-rpc.polkadot.io`
 
 // Statemint
-const DEV_ARCHIVE_URL = lookupArchive('statemint', { release: 'FireSquid' })
-const DEV_NODE_URL = 'wss://statemint-rpc.polkadot.io'
+// const DEV_ARCHIVE_URL = lookupArchive('statemint', { release: 'FireSquid' })
+// const DEV_NODE_URL = 'wss://statemint-rpc.polkadot.io'
 
-export const isProd = CHAIN === 'statemine'
+export const isProd = CHAIN !== 'rococo'
 
 console.log(`Using ${CHAIN} chain ${isProd ? 'production' : 'development'} environment`)
 
-export const getArchiveUrl = (): string => (isProd ? ARCHIVE_URL : DEV_ARCHIVE_URL)
-export const getNodeUrl = (): string => (isProd ? NODE_URL : DEV_NODE_URL)
+export const getArchiveUrl = (): string => ARCHIVE_URL
+export const getNodeUrl = (): string => NODE_URL
