04637918416f484efb0c673ff9569bbba7c37ed4
roiLeo <medina.leo42@gmail.com>
2023-12-18 18:01:50 +0100

    🔧 update entity on burn

--- src/mappings/uniques/burn.ts
+++ src/mappings/uniques/burn.ts
@@ -1,5 +1,5 @@
-import { getWith } from '@kodadot1/metasquid/entity'
-import { NFTEntity as NE } from '../../model'
+import { getOrFail } from '@kodadot1/metasquid/entity'
+import { NFTEntity as NE, CollectionEntity as CE } from '../../model'
 import { unwrap } from '../utils/extract'
 import { debug, pending, success } from '../utils/logger'
 import { Action, Context, createTokenId } from '../utils/types'
@@ -16,26 +16,28 @@ export async function handleTokenBurn(context: Context): Promise<void> {
   debug(OPERATION, event)
 
   const id = createTokenId(event.collectionId, event.sn)
-  const entity = await getWith(context.store, NE, id, { collection: true })
+  const entity = await getOrFail<NE>(context.store, NE, id)
+  const collection = await getOrFail<CE>(context.store, CE, event.collectionId)
 
   const { ownerCount, distribution } = await calculateCollectionOwnerCountAndDistribution(
     context.store,
-    entity.collection.id,
+    collection.id,
     entity.currentOwner
   )
 
   entity.burned = true
   entity.updatedAt = event.timestamp
 
-  entity.collection.updatedAt = event.timestamp
-  entity.collection.supply -= 1
-  entity.collection.ownerCount = ownerCount
-  entity.collection.distribution = distribution
+  collection.updatedAt = event.timestamp
+  collection.supply -= 1
+  collection.ownerCount = ownerCount
+  collection.distribution = distribution
 
   await burnHandler(context, entity)
 
   success(OPERATION, `${id} by ${event.caller}`)
   await context.store.save(entity)
+  await context.store.save(collection)
   const meta = entity.metadata ?? ''
   await createEvent(entity, OPERATION, event, meta, context.store)
 }
