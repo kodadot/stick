e839afee2226d76a8ae6384bf1f2f695a3c765a1
roiLeo <medina.leo42@gmail.com>
2024-01-11 16:14:53 +0100

    🔧 mint send_to

--- src/mappings/nfts/mint.ts
+++ src/mappings/nfts/mint.ts
@@ -70,9 +70,12 @@ export async function handleTokenCreate(context: Context): Promise<void> {
   success(OPERATION, `${final.id}`)
   await context.store.save(final)
   await context.store.save(collection)
-  await createEvent(final, OPERATION, event, '', context.store)
-
-  if (final.issuer !== final.currentOwner) {
-    await createEvent(final, Action.SEND, event, final.currentOwner, context.store, final.issuer)
-  }
+  await createEvent(
+    final,
+    OPERATION,
+    event,
+    final.issuer !== final.currentOwner ? final.currentOwner : '',
+    context.store,
+    final.issuer !== final.currentOwner ? final.issuer : undefined
+  )
 }
