a2dbb9536f1ba43df559db6761a128c3d3a06f26
Viki Val <viktorko99@gmail.com>
2024-01-17 18:06:13 +0100

    :wrench: Start block

--- src/environment.ts
+++ src/environment.ts
@@ -4,6 +4,10 @@ export type Chain = 'kusama' | 'rococo' | 'polkadot'
 
 export const CHAIN: Chain = process.env.CHAIN as Chain || 'kusama'
 
+const UNIQUE_STARTING_BLOCK = 323_750 // 618838;
+// const _NFT_STARTING_BLOCK = 4_556_552
+export const STARTING_BLOCK = UNIQUE_STARTING_BLOCK
+
 // Asset Hub
 const ARCHIVE_URL = lookupArchive(`asset-hub-${CHAIN}`, { release: 'ArrowSquid' })
 const NODE_URL = `wss://${CHAIN}-asset-hub-rpc.polkadot.io`
