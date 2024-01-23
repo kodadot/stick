23c825b3bd8fa48dfe81d71d41aac74e73fd8481
Viki Val <viktorko99@gmail.com>
2024-01-23 12:14:01 +0100

    :tada: :test_tube: unit tests

---
+++ tests/index.test.ts
@@ -0,0 +1,43 @@
+
+import { describe, expect, it, beforeAll, afterAll, test } from "vitest";
+import { onlyValue, addressOf } from "../src/mappings/utils/helper";
+
+describe("Helpers", () => {
+	// let store: SquidStore;
+
+	beforeAll(() => {
+    // store = new SquidStore();
+	});
+
+	afterAll(() => {});
+
+  describe("onlyValue", () => {
+    it("should return a value", () => {
+      const value = onlyValue({ __kind: "HolderOf", value: 1});
+      expect(value).toBe(1);
+    });
+
+    it("should not return a value", () => {
+      const value = onlyValue({ __kind: "Issuer" } as any);
+      expect(value).toBe(undefined);
+    });
+  })
+
+  describe("addressOf", () => {
+    it("should return a value", () => {
+      const value = addressOf('0x30658243d02e18551f0d447dad1065db75b01e570fd47eec86805dbea12a28ed');
+      expect(value).toBe('DfmyFcUiNkVPQdYZuiPLHFWDTSWGk6feN9aHe22ivbFAD6A');
+    });
+
+    test("should throw on invalid", () => {
+      expect.hasAssertions()
+      try {
+        addressOf('0x30658243d02e18551f0d447dad1065db75b01e570fd47eec86805dbea12a28e');
+      } catch (error) {
+        expect(error.message).toMatch('The expression evaluated to a falsy value');
+      }
+      
+    });
+  })
+
+});
