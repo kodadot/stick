cb5a1f80653792f4f625b50eb26b975fed7cfe30
Preschian Febryantara <preschian@users.noreply.github.com>
2024-04-06 16:07:08 +0700

    fix: timeout on fxart metadata

--- src/mappings/utils/metadata.ts
+++ src/mappings/utils/metadata.ts
@@ -1,7 +1,7 @@
 import { contentFrom, type Content } from '@kodadot1/hyperdata'
 import { ensure } from '@kodadot1/metasquid'
-import { EntityWithId, TokenMetadata } from '@kodadot1/metasquid/types'
-import { $obtain } from '@kodadot1/minipfs'
+import { EntityWithId } from '@kodadot1/metasquid/types'
+import { $obtain, type URI, obtain } from '@kodadot1/minipfs'
 import { MetadataEntity } from '../../model'
 import logger from './logger'
 // import { attributeFrom } from './types'
@@ -10,12 +10,22 @@ export const BASE_URL = 'https://image.w.kodadot.xyz/'
 /**
  * Fetch the metadata from the IPFS
  * @param metadata - the metadata to fetch
-**/
+ **/
 export const fetchMetadata = async <T extends Content>(metadata: string): Promise<T> => {
   try {
     if (!metadata) {
       return ensure<T>({})
     }
+
+    // e.g: https://fxart-beta.kodadot.workers.dev/metadata/v1/json?chain=ahp&collection=115&nft=2404413027&metadata=ipfs:%2F%2Fbafybeidwapzo6tll4qc2n4u7gmbjdett6fi37xis7dwzpacazxhjm6z7t4%2F0.json
+    if (metadata.includes('kodadot.workers.dev/metadata/')) {
+      const value = await obtain(metadata as URI, {
+        retry: 6,
+        retryDelay: 10_000,
+      })
+      return contentFrom(value as any) as T
+    }
+
     const value = await $obtain<T>(metadata, ['rmrk', 'infura_kodadot1'], true)
     return contentFrom(value as any) as T
   } catch (e) {
@@ -28,7 +38,7 @@ export const fetchMetadata = async <T extends Content>(metadata: string): Promis
 /**
  * Fetch the list of metadata from the IPFS
  * @param metadata - the metadata to fetch
-**/
+ **/
 export const fetchAllMetadata = async <T extends Content>(
   metadata: string[]
 ): Promise<(Partial<MetadataEntity> & EntityWithId)[]> => {
@@ -43,11 +53,8 @@ export const fetchAllMetadata = async <T extends Content>(
  * Format the metadata to be compatible with the cache model
  * @param id - the id of the metadata (CID)
  * @param metadata - the metadata to fetch
-**/
-export const makeCompatibleMetadata = (
-  id: string,
-  metadata: Content
-): Partial<MetadataEntity> & EntityWithId => ({
+ **/
+export const makeCompatibleMetadata = (id: string, metadata: Content): Partial<MetadataEntity> & EntityWithId => ({
   id,
   description: metadata.description || '',
   image: metadata.image || metadata.thumbnail,
