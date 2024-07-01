cc4b25cbec869672ae6e06486c2be5ae3e2a34ea
Viki Val <viktorko99@gmail.com>
2024-07-01 13:10:06 +0200

    feat: Update cancelSwap.ts to handle swap cancellation and update status

--- src/mappings/nfts/cancelSwap.ts
+++ src/mappings/nfts/cancelSwap.ts
@@ -1,9 +1,9 @@
-import { getWith } from '@kodadot1/metasquid/entity'
-import { NFTEntity as NE } from '../../model'
+import { getOrFail as get } from '@kodadot1/metasquid/entity'
+import { OfferStatus, Swap } from '../../model'
 import { createEvent } from '../shared/event'
 import { unwrap } from '../utils/extract'
 import { debug, pending, success } from '../utils/logger'
-import { Action, Context } from '../utils/types'
+import { Action, Context, createTokenId } from '../utils/types'
 import { getSwapCancelledEvent } from './getters'
 
 const OPERATION = Action.SEND
@@ -13,6 +13,15 @@ export async function handleCancelSwap(context: Context): Promise<void> {
   const event = unwrap(context, getSwapCancelledEvent)
   debug(OPERATION, event)
 
+  const id = createTokenId(event.collectionId, event.sn)
+  const entity = await get(context.store, Swap, id)
+
+  entity.status = OfferStatus.WITHDRAWN
+  entity.updatedAt = event.timestamp
+
+  success(OPERATION, `${id} by ${event.caller}`)
+
+  await context.store.save(entity)
   // SwapCancelled {
   //   offered_collection: T::CollectionId,
   //   offered_item: T::ItemId,
