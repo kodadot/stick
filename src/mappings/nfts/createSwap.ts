2dbb3c5e7e0c098000d247aee269cf9d029525b3
Viki Val <viktorko99@gmail.com>
2024-07-31 11:19:46 +0200

    :note: createSwap.ts

--- src/mappings/nfts/createSwap.ts
+++ src/mappings/nfts/createSwap.ts
@@ -28,7 +28,7 @@ export async function handleCreateSwap(context: Context): Promise<void> {
     warn(OPERATION, `Will be treated as **${Action.OFFER}**`)
     // TRUE_OPERATION = Action.OFFER
   }
-  // TODO: SWAP CAN BE OVERWRITTEN!
+  // DEV_NOT: SWAP CAN BE OVERWRITTEN!
   const id = createTokenId(event.collectionId, event.sn)
   const final = offer ? await getOrCreate(context.store, Offer, id, {}) : await getOrCreate(context.store, Swap, id, {})
   const deadline = BigInt(event.deadline)
